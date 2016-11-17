dc5e8202e19a5048ff8767eb340463ef83a769e0
james <jamesrochabrun@gmail.com>
2016-11-17 11:30:19 -0800

    commontools

---
+++ CoffeeMeetsBagel/Common.h
@@ -0,0 +1,20 @@
+//
+//  Common.h
+//  CoffeeMeetsBagel
+//
+//  Created by James Rochabrun on 11/17/16.
+//  Copyright © 2016 James Rochabrun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+static inline CGFloat width(UIView *view) { return view.bounds.size.width; }
+static inline CGFloat height(UIView *view) { return view.bounds.size.height; }
+static inline CGFloat originY(UIView *view) {return view.frame.origin.y; }
+static inline CGFloat originX(UIView *view) {return view.frame.origin.x; }
+
+@interface Common : NSObject
+
+@end
---
+++ CoffeeMeetsBagel/Common.m
@@ -0,0 +1,14 @@
+//
+//  Common.m
+//  CoffeeMeetsBagel
+//
+//  Created by James Rochabrun on 11/17/16.
+//  Copyright © 2016 James Rochabrun. All rights reserved.
+//
+
+#import "Common.h"
+
+
+@implementation Common
+
+@end
